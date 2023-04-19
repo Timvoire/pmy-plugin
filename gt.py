728044851ae89f3c5fe2811c90bf5dfccad28704
Timvoire <130999330+Timvoire@users.noreply.github.com>
2023-04-19 22:55:20 +0800

    修改增加语种检测，代码基于一佬修改

--- gt.py
+++ gt.py
@@ -1,26 +1,39 @@
 from pagermaid.listener import listener
 from pagermaid.single_utils import safe_remove
 from pagermaid.enums import Client, Message
-from pagermaid.utils import client
+from pagermaid.utils import client,pip_install
+import string
 import time
+
+pip_install("deep-translator")
+
+from deep_translator import GoogleTranslator
 
 
 async def get_text(message: Message):
     if reply := message.reply_to_message:
         if reply.text:
-            text = reply.text.replace("\n",',')
+            text = reply.text.replace("\n","")
             return text
     return ""
-
-
-async def translate(text):
-    uri = f'https://v.api.aa1.cn/api/api-fanyi-yd/index.php?msg={text}&type=3'
-    res = await client.get(uri, timeout=10.0)
-    if res.status_code == 200:
-        text = res.json()['text']
-        return text
+async def langdetect(text):
+    uri = 'https://fanyi.baidu.com/langdetect'
+    header = {
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+    }
+    data = {
+        'query': text
+    }
+    resp = await client.post(uri, headers=header, data=data)
+    if resp.status_code==200:
+        response = resp.json()
+        return response['lan']
     return ""
-
+    
+async def translate(tg, text):
+    language = {'zh': "en", 'en': 'zh-CN'}
+    translated = GoogleTranslator(source='auto', target=language[tg]).translate(text)
+    return translated
 
 @listener(command="gt",
           description="回复一句话进行翻译")
@@ -28,10 +41,11 @@ async def audio_to_voice(bot: Client, message: Message):
     text = await get_text(message)
     if text == '':
         return await message.edit("请回复一个文本信息")
-    else:
+    else:   
+        language = await langdetect(text)
         try:
-            result = await translate(text)
-            await message.edit(f"翻译结果：{result}")
+            result = await translate(language,text)
+            await message.edit(f"{result}")
         except Exception as e:
             await message.edit(f"翻译消息失败：{e}")
-
+    
