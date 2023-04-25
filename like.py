6a985da84a36f16e4d26e4189b243e461b97f390
Timvoire <130999330+Timvoire@users.noreply.github.com>
2023-04-25 23:38:13 +0800

    增加del方法

--- like.py
+++ like.py
@@ -2,24 +2,36 @@ from pagermaid.listener import listener
 from pagermaid.enums import Client, Message
 from pagermaid.single_utils import sqlite
 
+
 @listener(command="dz",
           description="点赞")
+async def dz_set(bot: Client, message: Message):
+    try:
+        if len(message.parameter) != 2:
+            await message.edit("格式错误")
+        else:
+            uid = message.parameter[-1]
+            if message.parameter[0] == "set":
+                sqlite[f'dz.{uid}'] = str(uid)
+                await message.edit("设置成功")
+            if message.parameter[0] == "del":
+                del sqlite[f'dz.{uid}']
+                await message.edit("删除成功")
+    except Exception as e:
+        return
 
-async def dz_set(bot:Client,message:Message):
-    uid = message.arguments
-    sqlite[f'dz.{uid}'] = str(uid)
-    await message.edit(uid)
 
 @listener(is_plugin=True, incoming=True, ignore_edited=True)
 async def dianzan(bot: Client, message: Message):
     try:
         cid = sqlite.get(f'dz.{message.from_user.id}', None)
-        await message.forward(str(cid))
+        if not cid:
+            return
         if message.from_user.id == int(cid):
             await bot.send_reaction(
                 chat_id=message.chat.id,
                 message_id=message.id,
-                emoji="🔥",
-        )
+                emoji="🐳",
+            )
     except Exception as e:
-        await message.edit(e[:200])
+        return
